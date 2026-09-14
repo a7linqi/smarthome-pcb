eb03299b3ad8e2edfe3c3e46e97ec6f35a53f4c5
李智豪 <2991235721@qq.com>
2026-09-14 17:42:07 +0800

    添加智能浇花手机控制端

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "jiaohua-dashboard-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./assets/plant.png",
+  "./assets/wendu.png",
+  "./assets/shidu.png",
+  "./assets/turanshidu.png",
+  "./assets/Water_pump_OFF.png",
+  "./assets/Water_pump_ON.png",
+  "./assets/baojing_OFF.png",
+  "./assets/baojing_ON.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
